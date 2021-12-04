95e5cfd193b887ce377a0c42b299769dfafcd824
sona815 <sonalipranam@gmail.com>
2021-12-03 22:19:40 -0500

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+// This class will print hello world!
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello,Sonali!");
+	}
+
+}
